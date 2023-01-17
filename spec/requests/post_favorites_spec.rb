bd8e25c64a07e1dbb06097b6c6e578d092782721
James White <whitejaad@gmail.com>
2023-01-17 10:26:46 -0800

    finished post favorites

--- spec/requests/post_favorites_spec.rb
+++ spec/requests/post_favorites_spec.rb
@@ -7,12 +7,19 @@ RSpec.describe 'Post Favorites' do
     post '/api/v1/users', headers: headers, params: body, as: :json
     @user = User.last
   end
+  
    it 'adds recipes to a favorite list for user' do
     headers = { 'CONTENT_TYPE' => 'application/json'}   
-    body = { 'api_key': "#{@user.api_key}", 'country': 'Thailand', 'recipe_link': '', 'recipe_title': '' }
+    body = { 'api_key': "#{@user.api_key}", 'country': 'Thailand', 'recipe_link': 'www.goodfood.com', 'recipe_title': 'amazing eats' }
     post '/api/v1/favorites', headers: headers, params: body, as: :json
 
     expect(response).to be_successful
     expect(response.status).to eq(201)
+
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed_response).to be_a(Hash)
+    expect(parsed_response).to have_key(:success)
+    expect(parsed_response[:success]).to eq('Favorite added successfully')
   end
 end
