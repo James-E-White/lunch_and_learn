0b898bbd80ab27222821a7ad365826a73e7be537
James White <whitejaad@gmail.com>
2023-01-17 17:36:48 -0800

    fixing postman'

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -3,6 +3,6 @@ class Favorite < ApplicationRecord
     validates :api_key, :country, :recipe_link, :recipe_title, presence: true
  end
 
- def self.find_my_favorites(api_key)
+   def self.find_my_favorites(api_key)
     Favorite.where(api_key: api_key)
 end
--- spec/requests/get_user_favorites_spec.rb
+++ spec/requests/get_user_favorites_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe 'get user Favorites' do
     post '/api/v1/users', headers: headers, params: body, as: :json
     @user = User.last
     
+    
   end
 
   it 'gets all of the favorites for a paritcular user' do
@@ -23,10 +24,36 @@ RSpec.describe 'get user Favorites' do
     expect(response.status).to eq(200)
 
     parsed_response = JSON.parse(response.body, symbolize_names: true)
+
     expect(parsed_response).to be_a Hash
     expect(parsed_response).to have_key(:data)
-
+    expect(parsed_response[:data]).to be_a(Array)
   end
+
+  it 'returns empty array fou user with no favorites' do
+    params = { 'api_key': "#{@user.api_key}" }
+    get "/api/v1/favorites", headers: @headers, params: params
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
+    expect(parsed_response).to be_a Hash
+    expect(parsed_response).to have_key(:data)
+    expect(parsed_response[:data]).to eq([])
+  end
+
+   it 'sends an error message with appropriate response if the api_key is invalid' do
+    params = { 'api_key': "nostringsonme" }
+    get "/api/v1/favorites", headers: @headers, params: params
+
+    expect(response.status).to eq(400)
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed_response[:error]).to eq("Error user not found")
+  end
+
+  
 end
 
 
